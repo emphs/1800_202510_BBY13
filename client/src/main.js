1cdc8a34b9ee1a5b924280ea49c9771e7aebd936
Larry <Ll_Larry.sjl@gmail.com>
2025-03-05 12:26:33 -0800

    update dependency, refactor entry point, add optional react

--- client/src/main.js
+++ client/src/main.js
@@ -1,1 +1,6 @@
-//TODO
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+import { auth } from './firebase';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
