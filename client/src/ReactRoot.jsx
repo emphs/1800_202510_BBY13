f96aecca553555bb4ea8b41c73d5cfe9a99bd855
Larry <Ll_Larry.sjl@gmail.com>
2025-03-05 16:59:06 -0800

    fix react not foound

--- client/src/ReactRoot.jsx
+++ client/src/ReactRoot.jsx
@@ -1,14 +1,19 @@
 
-import React from 'react'
+import React, {useEffect} from 'react'
 import { useState } from 'react'
 
 export default function ReactRoot({ children }) {
     const [count, setCount] = useState(0)
+    const [uhm, setUhm] = useState(false)
+
+    useEffect(() => {
+        setCount(count + uhm)
+    }, [count])
 
     return (
         <>
             <div style={{ position: 'fixed', top: "10%", left: "3%" }}>
-                <button onClick={() => setCount((count) => count + 1)}>
+                <button onClick={() => (setCount(count+1), setUhm(!uhm))}>
                     count is {count}
                 </button>
             </div>
--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ export default defineConfig({
         proxy: {
             '/api': 'http://localhost:3000'
         }
-    }
+    },
 })
