fb50916581c582426f7cc4b66457886ad54830fe
May <ymei7@my.bcit.ca>
2025-03-31 16:24:25 -0700

    build: finished building for 1.0 release and fixes firebase hosting. Everything is functional on the web now.

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-    server: {
-        proxy: {
-            '/api': 'http://localhost:3000'
-        }
+  root: '.', // Root where all HTML files live
+  publicDir: 'public', // Static assets from public/
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        dashboard: resolve(__dirname, 'dashboard.html'),
+        readmore: resolve(__dirname, 'readmore.html'),
+        webapp: resolve(__dirname, 'webapp.html'),
+        widget: resolve(__dirname, 'widget.html'),
+      },
     },
-})
+  },
+  base: '/', // Matches Firebase root
+});
